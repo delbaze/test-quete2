62908faed80793518c1e92b07829717813b39799
delbaze <56109313+delbaze@users.noreply.github.com>
2023-06-14 10:24:06 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     
     <div className="App">
-      <h1>COucou</h1>
+      <h1>Hello</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
